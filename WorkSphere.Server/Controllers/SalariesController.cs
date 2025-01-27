620239533d59801ff10adb776a0d0e4034efdf77
Udbasili <33573587+udabasili@users.noreply.github.com>
2025-01-26 21:48:27 -0400

    Generated Controllers

---
+++ WorkSphere.Server/Controllers/SalariesController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WorkSphere.Data;
+using WorkSphere.Server.Model;
+
+namespace WorkSphere.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalariesController : ControllerBase
+    {
+        private readonly WorkSphereDbContext _context;
+
+        public SalariesController(WorkSphereDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Salaries
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Salary>>> GetSalaries()
+        {
+            return await _context.
+                Salaries.
+                Include(s => s.Employee).
+                ToListAsync();
+        }
+
+        // GET: api/Salaries/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Salary>> GetSalary(int id)
+        {
+            var salary = await _context.Salaries.FindAsync(id);
+
+            if (salary == null)
+            {
+                return NotFound();
+            }
+
+            return salary;
+        }
+
+        // PUT: api/Salaries/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSalary(int id, Salary salary)
+        {
+            if (id != salary.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(salary).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SalaryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Salaries
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Salary>> PostSalary(Salary salary)
+        {
+            _context.Salaries.Add(salary);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSalary", new { id = salary.Id }, salary);
+        }
+
+        // DELETE: api/Salaries/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSalary(int id)
+        {
+            var salary = await _context.Salaries.FindAsync(id);
+            if (salary == null)
+            {
+                return NotFound();
+            }
+
+            _context.Salaries.Remove(salary);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SalaryExists(int id)
+        {
+            return _context.Salaries.Any(e => e.Id == id);
+        }
+    }
+}

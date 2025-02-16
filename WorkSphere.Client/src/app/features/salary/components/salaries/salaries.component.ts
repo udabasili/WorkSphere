27852272c5c5612e0dec47ec54cb7cdcf5c55c94
Udbasili <33573587+udabasili@users.noreply.github.com>
2025-02-16 16:01:55 -0400

    Created Salaries and Details component

---
+++ WorkSphere.Client/src/app/features/salary/components/salaries/salaries.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-salaries',
+  standalone: false,
+  
+  templateUrl: './salaries.component.html',
+  styleUrl: './salaries.component.css'
+})
+export class SalariesComponent {
+
+}
---
+++ WorkSphere.Client/src/app/features/salary/components/salary-details/salary-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-salary-details',
+  standalone: false,
+  
+  templateUrl: './salary-details.component.html',
+  styleUrl: './salary-details.component.css'
+})
+export class SalaryDetailsComponent {
+
+}

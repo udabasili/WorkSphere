419f0587405654e6e0425f8449b1536bd8a38a61
Udbasili <33573587+udabasili@users.noreply.github.com>
2025-02-16 16:00:44 -0400

    Started Manage Salary

---
+++ WorkSphere.Client/src/app/features/salary/components/manage-salary/manage-salary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-salary',
+  standalone: false,
+  
+  templateUrl: './manage-salary.component.html',
+  styleUrl: './manage-salary.component.css'
+})
+export class ManageSalaryComponent {
+
+}

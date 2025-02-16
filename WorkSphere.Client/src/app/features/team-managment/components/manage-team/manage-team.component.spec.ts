55e27edae47f788f79599d3da281dc11f3e82e9b
Udbasili <33573587+udabasili@users.noreply.github.com>
2025-02-16 16:01:09 -0400

    Updated Manage Team component

---
+++ WorkSphere.Client/src/app/features/team-managment/components/manage-team/manage-team.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ManageTeamComponent } from './manage-team.component';
+
+describe('ManageTeamComponent', () => {
+  let component: ManageTeamComponent;
+  let fixture: ComponentFixture<ManageTeamComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ManageTeamComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ManageTeamComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ WorkSphere.Client/src/app/features/team-managment/components/manage-team/manage-team.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-team',
+  standalone: false,
+  
+  templateUrl: './manage-team.component.html',
+  styleUrl: './manage-team.component.css'
+})
+export class ManageTeamComponent {
+
+}
